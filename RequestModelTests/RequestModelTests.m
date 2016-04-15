ed820032e111dcee58778cb0c4ba2b8ba54cef50
mikezone <82643885@qq.com>
2016-04-15 11:28:43 +0800

    Initial Commit

---
+++ RequestModelTests/RequestModelTests.m
@@ -0,0 +1,39 @@
+//
+//  RequestModelTests.m
+//  RequestModelTests
+//
+//  Created by Mike on 16/4/15.
+//  Copyright © 2016年 Mike. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RequestModelTests : XCTestCase
+
+@end
+
+@implementation RequestModelTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
